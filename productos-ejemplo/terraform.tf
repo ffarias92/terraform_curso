56cbd1e696421d3956baadc92e8127af30a86903
Pipe <ffariasg92@gmail.com>
2023-12-31 17:26:19 -0300

    agregado orden de carpetas

---
+++ productos-ejemplo/terraform.tf
@@ -0,0 +1,11 @@
+resource "local_file" "productos" {
+    content = "lista de productos"
+    filename = "productos.txt"
+}
+
+
+# terraform init : ejecutar recurso con proveedor terraform por primera vez 
+# terraform plan : revisar lo que ejecutara terraform
+# terraform apply : aplicar terraform
+# terraform destroy : destruir todos los cambios realizados con terraform
+# terraform show : muestra la configuracion aplicada actual (imprime ordenadamente el archivo terraform.tfstate)
